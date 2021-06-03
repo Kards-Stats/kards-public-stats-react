6f61fa8f797ec0f0a738d4fa5a1c755cba57a0f6
KlutzyBubbles <LTzilantonis@gmail.com>
2021-06-04 09:20:15 +1000

    try with different json tree package

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import Logo from '../assets/logo.svg';
 
 import _ from 'underscore';
 
-import ReactJson from 'react-json-view';
+import JSONTree from 'react-json-tree'
 
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
@@ -19,6 +19,27 @@ const client = new ApolloClient({
 
 const nameReg = /^[a-zA-Z0-9-_]+[#]\d{4}$/;
 const idReg = /^\d{6}$/;
+
+const jsonTheme = {
+  scheme: 'monokai',
+  author: 'wimer hazenberg (http://www.monokai.nl)',
+  base00: '#272822',
+  base01: '#383830',
+  base02: '#49483e',
+  base03: '#75715e',
+  base04: '#a59f85',
+  base05: '#f8f8f2',
+  base06: '#f5f4f1',
+  base07: '#f9f8f5',
+  base08: '#f92672',
+  base09: '#fd971f',
+  base0A: '#f4bf75',
+  base0B: '#a6e22e',
+  base0C: '#a1efe4',
+  base0D: '#66d9ef',
+  base0E: '#ae81ff',
+  base0F: '#cc6633',
+};
 
 export default class App extends Component {
   /** Gets fired when the route changes.
@@ -311,7 +332,7 @@ export default class App extends Component {
             <br/>
             <br/>
             <h3>Raw Data</h3>
-            <ReactJson src={this.state.data} theme='monokai' collapsed='true'/>
+            <JSONTree data={this.state.data} theme={jsonTheme} invertTheme={false}/>
           </div>;
         } else {
           let text;
@@ -366,7 +387,7 @@ export default class App extends Component {
             <br/>
             <br/>
             <h3>Raw Data</h3>
-            <ReactJson src={this.state.data} theme='monokai' collapsed='true'/>
+            <JSONTree data={this.state.data} theme={jsonTheme} invertTheme={false}/>
           </div>;
         }
       }
